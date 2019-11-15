5ca05652a84aa0248d6890ca94f98f1920e6918a
HABUMUGISHA Fabrice <fabrice.habumugisha@solidor.malo>
2019-11-15 16:25:48 +0100

    initialize projects listing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { ProjectListComponent } from './projects/project-list/project-list.component';
 import {AuthGuard} from './auth/auth.guard';
+import { ProjectCreateComponent } from './projects/project-create/project-create.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'projects' },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'projects', component: ProjectListComponent, canActivate: [AuthGuard] },
+  { path: 'projects/new', component: ProjectCreateComponent, canActivate: [AuthGuard] },
 ];
 
 @NgModule({
--- src/app/projects/project-list/project-list.component.ts
+++ src/app/projects/project-list/project-list.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectListComponent implements OnInit {
 
+  projects: [] = [];
+
   constructor() { }
 
   ngOnInit() {
