b7cf1fb3202d960d83d0c51afe1865f602fc69c6
HABUMUGISHA <fabrice.a.habumugisha@capgemini.com>
2020-01-09 08:46:52 +0100

    sanitize filemane pdf

--- src/app/projects/project-list/project-list.component.ts
+++ src/app/projects/project-list/project-list.component.ts
@@ -3,12 +3,18 @@ import { ProjectService } from "./../project.service";
 import { Component, OnInit, OnDestroy } from "@angular/core";
 import { Project } from "src/app/projects/project.model";
 import { Subscription } from "rxjs";
-import { Router, ActivatedRoute } from '@angular/router';
-import { PageEvent, MatSnackBar, MatDialogRef, MatDialog } from '@angular/material';
-import pdfMake from 'pdfmake/build/pdfmake';
-import pdfFonts from 'pdfmake/build/vfs_fonts';
-import { ConfirmDialogComponent } from 'src/app/shared/confirm-dialog/confirm-dialog.component';
-import { ErrorDialogComponent } from 'src/app/shared/error/error-dialog/error-dialog.component';
+import { Router, ActivatedRoute } from "@angular/router";
+import {
+  PageEvent,
+  MatSnackBar,
+  MatDialogRef,
+  MatDialog
+} from "@angular/material";
+import pdfMake from "pdfmake/build/pdfmake";
+import pdfFonts from "pdfmake/build/vfs_fonts";
+import { ConfirmDialogComponent } from "src/app/shared/confirm-dialog/confirm-dialog.component";
+import { ErrorDialogComponent } from "src/app/shared/error/error-dialog/error-dialog.component";
+import { sanitize } from "sanitize-filename-ts";
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
 @Component({
   selector: "app-project-list",
@@ -26,114 +32,100 @@ export class ProjectListComponent implements OnInit, OnDestroy {
 
   confirmDeleteDialogRef: MatDialogRef<ConfirmDialogComponent>;
   isLoading = false;
-  constructor(private projectService: ProjectService,
+  constructor(
+    private projectService: ProjectService,
     private router: Router,
     private route: ActivatedRoute,
     public snackBar: MatSnackBar,
-    private dialog: MatDialog,) {}
+    private dialog: MatDialog
+  ) {}
 
   ngOnInit() {
-    this.projectService.getProjects(this.projectsPerPage, this.currentPage)
-    .subscribe((data : {projects: Project[], totalItems: number}) => {
-      this.projects = data.projects;
-      this.totalProjects = data.totalItems;
-
-    });
-
+    this.projectService
+      .getProjects(this.projectsPerPage, this.currentPage)
+      .subscribe((data: { projects: Project[]; totalItems: number }) => {
+        this.projects = data.projects;
+        this.totalProjects = data.totalItems;
+      });
   }
 
   onEdit(projectId: string) {
     console.log(projectId);
-    this.router.navigate(['/projects', projectId], {relativeTo : this.route});
+    this.router.navigate(["/projects", projectId], { relativeTo: this.route });
   }
-
 
   openSnackBar(message: string, action: string) {
     this.snackBar.open(message, action, {
-       duration: 2000,
-    });
- }
-
-  onChangedPage(pageData: PageEvent) {
-   // this.isLoading = true;
-    this.currentPage = pageData.pageIndex + 1;
-    this.projectsPerPage = pageData.pageSize;
-    this.projectService.getProjects(this.projectsPerPage, this.currentPage)
-    .subscribe((data : {projects: Project[], totalItems: number}) => {
-      this.projects = data.projects;
-      this.totalProjects = data.totalItems;
-
+      duration: 2000
     });
   }
 
-  generatePdf(projectId: string){
+  onChangedPage(pageData: PageEvent) {
+    // this.isLoading = true;
+    this.currentPage = pageData.pageIndex + 1;
+    this.projectsPerPage = pageData.pageSize;
+    this.projectService
+      .getProjects(this.projectsPerPage, this.currentPage)
+      .subscribe((data: { projects: Project[]; totalItems: number }) => {
+        this.projects = data.projects;
+        this.totalProjects = data.totalItems;
+      });
+  }
+
+  generatePdf(projectId: string) {
     this.projectService.getProject(projectId).subscribe(data => {
-      const project: Project  =  data;
+      const project: Project = data;
       const documentDefinition = {
         content: [
-        {
-          text: project.title,
-          bold: true,
-          fontSize: 20,
-          alignment: 'center',
-
-        }
-      ] };
-      pdfMake.createPdf(documentDefinition).download();
+          {
+            text: project.title,
+            bold: true,
+            fontSize: 20,
+            alignment: "center"
+          }
+        ]
+      };
+      const filename = sanitize(project.title);
+      pdfMake.createPdf(documentDefinition).download(filename);
       //https://www.ngdevelop.tech/angular-8-export-to-pdf-using-pdfmake/
       //https://www.ngdevelop.tech/export-to-excel-in-angular-6/
     });
-
-
   }
 
   onDelete(projectId: string) {
-
     this.confirmDeleteDialogRef = this.dialog.open(ConfirmDialogComponent, {
       hasBackdrop: false,
-      data : {
-        elementToDelete: 'your project',
-
+      data: {
+        elementToDelete: "your project"
       }
     });
 
     this.confirmDeleteDialogRef
-    .afterClosed()
-    .subscribe((responseData: boolean) => {
-      if (responseData ) {
+      .afterClosed()
+      .subscribe((responseData: boolean) => {
+        if (responseData) {
           this.isLoading = true;
-          this.projectService.deleteProject(projectId)
-          .subscribe(result => {
-            this.projects = this.projects.filter(p => p._id !== projectId);
-            this.totalProjects = this.totalProjects - 1;
-            this.openSnackBar(result.message, 'Ok');
+          this.projectService.deleteProject(projectId).subscribe(
+            result => {
+              this.projects = this.projects.filter(p => p._id !== projectId);
+              this.totalProjects = this.totalProjects - 1;
+              this.openSnackBar(result.message, "Ok");
+            },
+            errorData => {
+              console.log(errorData.message);
 
-          },
-          errorData => {
-            console.log(errorData.message);
-
-            this.isLoading = false;
-            this.dialog.open(ErrorDialogComponent, {
-              hasBackdrop: true,
-              data : {
-                message : errorData.message,
-                data : errorData.data
-              }
-            });
-
-          });
-
-
+              this.isLoading = false;
+              this.dialog.open(ErrorDialogComponent, {
+                hasBackdrop: true,
+                data: {
+                  message: errorData.message,
+                  data: errorData.data
+                }
+              });
+            }
+          );
         }
-
-
-
-    });
-
-
-
-
-
+      });
   }
 
   ngOnDestroy() {
