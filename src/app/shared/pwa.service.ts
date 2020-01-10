97ff4abea805c50dbb1becf3e069af17cce6ede6
fhabumugisha <fabriceboris@gmail.com>
2020-01-10 16:49:49 +0100

    add pwa

--- src/app/shared/pwa.service.ts
+++ src/app/shared/pwa.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
 import { SwUpdate } from '@angular/service-worker';
 @Injectable({
-  providedIn: "root"
+  providedIn: 'root'
 })
 export class PwaService {
   constructor(private updates: SwUpdate) {
     updates.available.subscribe(event => {
       console.log(event);
-      if (confirm("New version available. Load New Version?")) {
+      if (confirm('New version available. Load New Version?')) {
         updates.activateUpdate().then(() => document.location.reload());
       }
     });
