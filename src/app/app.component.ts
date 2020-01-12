601c42aeed6655f612bcd0eac18d042e597dd199
fhabumugisha <fabriceboris@gmail.com>
2020-01-12 23:02:13 +0100

    update pwa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,36 @@
-import { AuthService } from "./auth/auth.service";
-import { Component, OnInit } from "@angular/core";
-import { TranslateService } from "@ngx-translate/core";
-import { SwUpdate } from "@angular/service-worker";
+import { AuthService } from './auth/auth.service';
+import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { SwUpdate } from '@angular/service-worker';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = "calcapp-ngclient";
+  title = 'Calcapp';
   language: string;
   constructor(
     private authService: AuthService,
     private translate: TranslateService,
-    private updates: SwUpdate
+    private updates: SwUpdate,
+    public snackBar: MatSnackBar,
   ) {
     updates.available.subscribe(event => {
       console.log(event);
-      if (confirm("New version available. Load New Version?")) {
-        updates.activateUpdate().then(() => document.location.reload());
-      }
+      const snackBarRef  = this.snackBar.open('New version available', 'Update');
+      snackBarRef.onAction().subscribe(() => {
+      console.log('The snack-bar action was triggered!');
+      updates.activateUpdate().then(() => document.location.reload());
+    });
+
     });
   }
 
   ngOnInit(): void {
-    this.language = "en";
+    this.language = 'en';
     this.authService.autoLogin();
   }
 
